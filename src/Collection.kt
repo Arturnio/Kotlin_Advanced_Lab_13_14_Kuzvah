72c7bc64795e358a726787287120a660648aa024
artur <only.up.ak@gmail.com>
2026-01-21 09:45:47 +0300

    Sets in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -14,7 +14,6 @@ val newSolarSystem=arrayOf(
 
 )
 fun main() {
-//    println(solarSystem[7])
     val solarSystem = mutableListOf(
         "Mercury",
         "Venus",
@@ -25,17 +24,33 @@ fun main() {
         "Uranus",
         "Neptune"
     )
-    solarSystem.add("Pluto")
-    solarSystem.add(3,"Theia")
-    solarSystem[3]="Future Moon"
+//    solarSystem.remove("Pluto")
+//    println(solarSystem.size)
+//    println(solarSystem.contains("Pluto"))
+
+//    println("Pluto" in solarSystem)
+//    solarSystem.add("Pluto")
+//    println(solarSystem.size)
+
+//    println(solarSystem.size)
+//    solarSystem.add("Pluto")
+//    println(solarSystem.size)
+//    println(solarSystem.contains("Pluto"))
+
+
+
+//    println(solarSystem[7])
+//    solarSystem.add("Pluto")
+//    solarSystem.add(3,"Theia")
+//    solarSystem[3]="Future Moon"
 //    println(solarSystem[3])
 //    println(solarSystem[9])
 //    solarSystem.removeAt(9)
 //    println(solarSystem.size)
 //    println(solarSystem.indexOf("Earth"))
 //    println(solarSystem.indexOf("Pluto"))
-    println(solarSystem.contains("Pluto"))
-    println("Future Moon" in solarSystem)
+//    println(solarSystem.contains("Pluto"))
+//    println("Future Moon" in solarSystem)
 //for (planet in solarSystem){
 //    println(planet)
 //}
